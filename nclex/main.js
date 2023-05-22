a250885f3cf6f65b3914401b1c75c8d0a9168b2e
Talank <talank@gces.edu.np>
2023-05-22 01:51:28 -0400

    Fixed scoring criteria

--- nclex/main.js
+++ nclex/main.js
@@ -9,6 +9,7 @@ let questionTimer; // This will hold the question timer
 let overallTimer; // This will hold the overall timer
 let questionTime; // This will hold the question time
 let overallTime; // This will hold the overall time
+let submittedAnswerCount;
 
 function startTimers() {
     overallTime = 0;
@@ -58,20 +59,29 @@ function showQuestion() {
         li.addEventListener('click', () => selectOption(i));
         optionsElement.appendChild(li);
     }
+
+    selectedOption = -1; // Reset selected option
 }
 
 
 // This function will handle the logic when an option is clicked
 function selectOption(optionIndex) {
+    // Remove the selected class from the previously selected option
+    if (selectedOption != -1) {
+        document.getElementById('options').children[selectedOption].classList.remove('selected');
+    }
+
     selectedOption = optionIndex;
+    // Add the selected class to the new selected option
+    document.getElementById('options').children[selectedOption].classList.add('selected');
 }
 
-// This function will handle the logic when the answer is submitted
 // This function will handle the logic when the answer is submitted
 function submitAnswer() {
     const optionsElement = document.getElementById('options');
     const correctOption = optionsElement.children[questions[currentIndex].answer];
     const selectedOptionElement = optionsElement.children[selectedOption];
+    submittedAnswerCount++;
 
     if (selectedOption === questions[currentIndex].answer) {
         correctAnswers++;
@@ -107,22 +117,29 @@ function submitAnswer() {
 
 // This function will handle the logic when the "Next Question" button is clicked
 function nextQuestion() {
+    currentIndex++;
     stopQuestionTimer();
 
-    // Get all questions of the current difficulty level
-    let filteredQuestions = questions.filter(question => question.difficulty_level === difficultyLevel);
-    
-    // If there are no questions at the current difficulty level, the user has answered all questions of this difficulty
-    if (filteredQuestions.length === 0) {
+    if (currentIndex < questions.length) {
+        startQuestionTimer();
+        showQuestion();
+        document.getElementById('rationaleContainer').style.display = 'none';
+        document.getElementById('nextQuestionButton').style.display = 'none';
+        document.getElementById('submitAnswerButton').style.display = 'inline-block';
+    } else {
+        // The user has answered all the questions
+        stopQuestionTimer();
+        clearInterval(overallTimer);
+        
         // Now we check the user's score to determine whether they passed or failed
-        let score = correctAnswers / questions.length * 100;
+        let score = correctAnswers / (submittedAnswerCount) * 100; // Update score calculation
         if (score >= 70) {
             alert('Congratulations, you passed the test with a score of ' + score + '%!');
         } else {
             alert('Sorry, you did not pass the test. Your score was ' + score + '%.');
         }
-        return;
     }
+}
     
     // Randomly select a question of the current difficulty level
     let randomIndex = Math.floor(Math.random() * filteredQuestions.length);
@@ -139,7 +156,10 @@ function nextQuestion() {
 function startTest() {
     document.getElementById('startTestButton').style.display = 'none';
     document.getElementById('testContainer').style.display = 'block';
+    submittedAnswerCount=0;
     startTimers();
     loadJson();
 }
+
+// Add a bar to show how many answers are correct so fat, and what percentage of questions are correct. I added a variable 
 
